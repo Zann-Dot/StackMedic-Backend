a751f96dfc51cb5b826048ec2bf96f862de4bc2e
Zann-Dot <anaykarn234@gmail.com>
2026-09-23 00:40:51 +0530

    feat: add project router with authentication and project creation logic

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ import cors from "cors";
 import { zodResponseFormat } from "openai/helpers/zod";
 import z from "zod";
 import { OpenAI } from "openai/client.js";
-import errorLogModel from "./models/errorLog.model.js";
+import projectRouter from "./routes/project.route.js";
+import { clerkMiddleware, getAuth } from "@clerk/express"
+
 // connectDB();
+configDotenv();
 const PORT = 3000;
 const app = express();
 const corsConfig = {
@@ -16,7 +19,7 @@ const corsConfig = {
 }
 app.use(express.json());
 app.use(cors(corsConfig));
-configDotenv();
+app.use(clerkMiddleware());
 app.use(async (req, res, next) => {
     try {
         await connectDB();
@@ -26,83 +29,86 @@ app.use(async (req, res, next) => {
     }
 });
 
-app.post("/api/error-log-analytics", async (req, res) => {
-    try {
-        const { rawError } = req.body;
-        const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+// app.post("/api/error-log-analytics", async (req, res) => {
+//     try {
+//         const { rawError } = req.body;
+//         const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-        if (!rawError)
-            return res.status(400).json({ error: "No error trace provided" });
+//         if (!rawError)
+//             return res.status(400).json({ error: "No error trace provided" });
 
-        const DiagnosticSchema = z.object({
-            errorType: z.enum([
-                "Syntax",
-                "Runtime",
-                "Network",
-                "Database",
-                "Unknown",
-            ]),
-            codeFix: z
-                .string()
-                .describe("The corrected code snippet safely fixing the bug"),
-            explanation: z
-                .string()
-                .describe("A clear 2-sentence breakdown of what went wrong"),
-            description: z
-                .string()
-                .describe("A short one line description of what went wrong"),
-            title: z
-                .string()
-                .describe("A short title max 2-3 words justifing the error log"),
-            language: z
-                .string()
-                .describe("The programming language of the raw code error"),
-        });
+//         const DiagnosticSchema = z.object({
+//             errorType: z.enum([
+//                 "Syntax",
+//                 "Runtime",
+//                 "Network",
+//                 "Database",
+//                 "Unknown",
+//             ]),
+//             codeFix: z
+//                 .string()
+//                 .describe("The corrected code snippet safely fixing the bug"),
+//             explanation: z
+//                 .string()
+//                 .describe("A clear 2-sentence breakdown of what went wrong"),
+//             description: z
+//                 .string()
+//                 .describe("A short one line description of what went wrong"),
+//             title: z
+//                 .string()
+//                 .describe("A short title max 2-3 words justifing the error log"),
+//             language: z
+//                 .string()
+//                 .describe("The programming language of the raw code error"),
+//         });
 
-        const completion = await openai.chat.completions.parse({
-            model: "gpt-4o-mini",
-            messages: [
-                {
-                    role: "system",
-                    content: `You are a senior full-stack compiler diagnostics engine. When providing code in the 'codeFix' field: 
-                            1. You MUST format it as readable, beautifully indented code. 
-                            2. Use '\\n' for every new line and explicit double spaces or tabs for nested blocks. 
-                            3. Do NOT output the code as a single-line string. 
-                            4. Send the whole corrected code as output not just the specific line that has issue.
-                            5. Check for any type of redundancy or potential bugs in the code, and return the most likely fixed output.
-                        `,
-                },
-                {
-                    role: "user",
-                    content: rawError,
-                },
-            ],
-            response_format: zodResponseFormat(DiagnosticSchema, "diagnostic"),
-        });
+//         const completion = await openai.chat.completions.parse({
+//             model: "gpt-4o-mini",
+//             messages: [
+//                 {
+//                     role: "system",
+//                     content: `You are a senior full-stack compiler diagnostics engine. When providing code in the 'codeFix' field: 
+//                             1. You MUST format it as readable, beautifully indented code. 
+//                             2. Use '\\n' for every new line and explicit double spaces or tabs for nested blocks. 
+//                             3. Do NOT output the code as a single-line string. 
+//                             4. Send the whole corrected code as output not just the specific line that has issue.
+//                             5. Check for any type of redundancy or potential bugs in the code, and return the most likely fixed output.
+//                         `,
+//                 },
+//                 {
+//                     role: "user",
+//                     content: rawError,
+//                 },
+//             ],
+//             response_format: zodResponseFormat(DiagnosticSchema, "diagnostic"),
+//         });
 
-        const aiAnalysis = completion.choices[0].message.parsed;
+//         const aiAnalysis = completion.choices[0].message.parsed;
 
-        const newLog = await errorLogModel.create({
-            ...aiAnalysis,
-            rawError,
-        });
+//         const newLog = await errorLogModel.create({
+//             ...aiAnalysis,
+//             rawError,
+//         });
 
-        res.status(201).json(newLog);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+//         res.status(201).json(newLog);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
 
-app.get("/api/get-error-log", async (req, res) => {
-    try {
-        const errorLog = await errorLogModel.find({});
-        if (!errorLog || errorLog === [])
-            return res.status(404).json({ error: "Error logs not found" });
-        res.json(errorLog);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+// app.get("/api/get-error-log", async (req, res) => {
+//     try {
+//         const errorLog = await errorLogModel.find({});
+//         if (!errorLog || errorLog === [])
+//             return res.status(404).json({ error: "Error logs not found" });
+//         res.json(errorLog);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
+
+app.use("/api", projectRouter);
 
 app.listen(PORT, () => {
     console.log(`server is running on port: ${PORT}`);
---
+++ routes/project.route.js
@@ -0,0 +1,45 @@
+import { clerkClient, getAuth } from "@clerk/express";
+import express from "express"
+import { Project } from "../models/Project";
+const projectRouter = express.Router();
+
+projectRouter.post("/projects", async (req, res) => {
+    try {
+        const { isAuthenticated, userId } = getAuth(req)
+        const { name, description } = req.body;
+        if (!name || !description)
+            return res.status(404).json({ error: "Please provide metadata for project." });
+
+        if (!isAuthenticated || !userId)
+            return res.status(403).json({ error: "Unauthorized" });
+
+        const email = (await clerkClient.users.getUser(userId)).primaryEmailAddress
+
+        const members = [
+            {
+                clerkUserId: userId,
+                email,
+                role: "admin"
+            }
+        ]
+
+        const metadata = await Project.create({
+            name,
+            description,
+            ownerClerkId: userId,
+            apiKey: "daa",
+            members
+        })
+
+        res.json({
+            success: true,
+            message: "Project created successfully",
+            metadata
+        })
+
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+export default projectRouter
